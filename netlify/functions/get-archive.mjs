68130bced84132266a2f0f723ff18fec135651fc
dkb754 <duane@culinarycoach.org>
2026-03-15 12:21:54 -0400

    Delete netlify/functions directory

--- netlify/functions/get-archive.mjs
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, context) {
-  const headers = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"
-  };
-  return new Response(JSON.stringify([]), { status: 200, headers });
-}
-
-export const config = { path: "/api/archive" };
